d43ea746252493c0de05137adcc98abc280b8404
Piotr Buchman <piotr.buchman@ssp26.pl>
2023-12-20 14:01:38 +0100

    Dodano eliminacje gaussa

--- src/gauss.c
+++ src/gauss.c
@@ -1,14 +1,40 @@
 #include "gauss.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
 
+	int nr = mat->r;
+	int nc = mat->c;
+
+	double *rzedy = malloc(sizeof(double) * nr);
+	double *wyz_r = malloc(sizeof(double) * nr);
+
+	for(int k = 0; k < nc; k++)
+	{
+  		for(int i = 0; i < nr; i++)
+			rzedy[i] = mat->data[i][k];
+	
+		for(int j = 0; j < nr; j++)
+		{
+			wyz_r[j] = rzedy[k+j+1] / rzedy[k];
+
+			for(int l=0; l < nr; l++)
+			{
+				for(int p = 0; p < nc; p++)
+				{
+					if (k + p + 1 < nc && l < nr) 
+					{
+                        mat->data[k+p+1][l] -= mat->data[k+p+1][l] * wyz_r[p];
+					}
+				}
+			}
+		}
+	}
 		return 0;
 }
 
