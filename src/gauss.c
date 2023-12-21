0726bc33141cf39216683eddfed162a1fb3a8026
Piotr Buchman <piotr.buchman@ssp26.pl>
2023-12-21 18:06:41 +0100

    wybieranie lemementu + zamiana miejsc

--- src/gauss.c
+++ src/gauss.c
@@ -1,6 +1,7 @@
 #include "gauss.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
@@ -8,10 +9,61 @@
  */
 int eliminate(Matrix *mat, Matrix *b){
 
+
+
+
 	double* wyznacznik = malloc(sizeof(double)*mat->r);
 	double* wiersz = malloc(sizeof(double)*mat->c);
+
 	for(int wk = 0; wk < mat->r; wk++)
 	{
+
+	double* kolumna = malloc(sizeof(double)*mat->r);
+
+	for(int i = wk; i < mat->r; i++)
+		kolumna[i] = mat->data[i][wk];
+
+	double max = fabs(kolumna[wk]);
+	int k = wk;
+
+	for(int j = wk+1; j < mat->r; j++)
+	{
+		double cur = fabs(kolumna[j]);
+		printf("%lf\n", cur); /* kkk*/
+		if (cur > max)
+		{
+			max = cur;
+			k = j;
+		}
+	}
+	double* tmp_w = malloc(sizeof(double)*mat->c);
+	for(int x= wk; x < mat->c; x++)
+	{
+		if(k == wk)
+			break;
+
+
+		for (int y = wk; y < mat->c; y++)
+			tmp_w[y] = mat->data[wk][y];
+
+		mat->data[wk][x] = mat->data[k][x];
+		mat->data[k][x] = tmp_w[x];	
+
+	}
+		free(tmp_w);
+		free(kolumna);
+		double* tmp_b = malloc(sizeof(double)*mat->c);
+
+	if(k != wk )
+	{
+		for (int y = wk; y < b->r; y++)
+			tmp_b[y] = b->data[y][0];
+
+		b->data[wk][0] = b->data[k][0];
+		b->data[k][0] = tmp_b[wk];
+	}
+	printToScreen(mat);
+	printToScreen(b);
 		double elem = mat->data[wk][wk];
 
 		for(int i = wk+1; i < mat->r; i++)
@@ -30,6 +82,7 @@ int eliminate(Matrix *mat, Matrix *b){
 	}
     free(wyznacznik);
     free(wiersz);
+
 	
 	return 0;
 }
